60d764fe98e464fcb804916f5d98a838f4c55689
Krasva687 <krasva687@gmail.com>
2021-09-19 17:42:11 +0300

    added functional

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -1,10 +1,12 @@
 package ru.sber.functional
 
+import kotlin.math.pow
+
 object PowFactory {
     /**
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
      */
-    fun buildPowFunction(pow: Int) {
-        TODO()
+    fun buildPowFunction(exponent: Int): (Int) -> Int {
+        return {value -> value.toDouble().pow(exponent).toInt()}
     }
 }
--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -1,10 +1,25 @@
 package ru.sber.functional
 
-class StudentsGroup {
+class StudentsGroup(firstName: List<String>,lastName: List<String>, averageRate: List<Double>) {
 
-    lateinit var students: List<Student>
+    var students: List<Student>
 
-    fun filterByPredicate() {
-        TODO()
+    fun filterByPredicate(value: (Student) -> Boolean): List<Student> {
+        if(students != null){
+            return students.filter{oneStudent -> value(oneStudent)}
+        }
+        return listOf()
+    }
+
+    init {
+        val group = ArrayList<Student>()
+        for(i in firstName.indices){
+            group.add(Student(
+                firstName = firstName[i],
+                lastName = lastName[i],
+                averageRate = averageRate[i]
+            ))
+        }
+        students = group
     }
 }
