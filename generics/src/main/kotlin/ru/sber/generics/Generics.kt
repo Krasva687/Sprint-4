41b85aac10f4ce27428d1c4ac88fca3f13166895
Krasva687 <krasva687@gmail.com>
2021-09-26 15:52:46 +0300

    Added Generics

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,41 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
-
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <T> compare(p1: Pair<T, T>, p2: Pair<T, T>): Boolean {
+
+    return p1.first == p2.first && p2.second == p1.second
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <E : Comparable<E>> countGreaterThan(anArray: Array<E>, elem: E): Int {
+    var cntGreaterNums = 0
+    anArray.forEach {
+        if(it > elem) cntGreaterNums++
+    }
+    return cntGreaterNums
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter <T: Comparable<T>> {
+    val list: MutableList<T> = mutableListOf()
+
+    fun add(value: T) {
+        list.add(value)
+        list.sort()
+    }
 }
 
 // 4.
-class Stack {
+class Stack <T> {
+    val list: MutableList<T> = mutableListOf()
 
+    fun push(elem: T){
+        list.add(0,elem)
+    }
+
+    fun pop(): T {
+        return list.removeAt(0)
+    }
+
+    fun isEmpty():Boolean = list.isNullOrEmpty()
 }
